90f0ac82d649cd8f29837a4c2f31a6eccdd48614
dhoni <alveox@gmail.com>
2024-10-21 14:12:26 +0700

    change healthcheck on nextcloud.yml fixing typo on setup.sh

--- setup.sh
+++ setup.sh
@@ -7,15 +7,15 @@ sed -i "s/aaa@bbb.ccc/$varmail/" traefik.yml
 sed -i "s/hostname.domain.TLD/$varfqdn/" dynamic/nextcloud.yml
 
 echo Creating docker network
-docker network create --driver=bridge proxy <br/>
-docker network create --driver=bridge nextcloud-aio <br/>
+docker network create --driver=bridge proxy
+docker network create --driver=bridge nextcloud-aio
 
 echo Creating traefik stack with letsencrypt using email: $varmail
-docker compose -p traefik -f docker-compose-traefik.yml up -d <br/>
+docker compose -p traefik -f docker-compose-traefik.yml up -d
 
 echo Creating nextcloud stack for $varfqdn
-docker compose -p nextcloud_aio -f docker-compose-nextcloud_aio.yml up -d <br/>
-
+docker compose -p nextcloud_aio -f docker-compose-nextcloud_aio.yml up -d
 
 echo Done
-echo Proced nextcloud installation on your ip:8080
+echo Proced nextcloud installation on your ip:8081
+echo For monitoring traefik open ip:8080

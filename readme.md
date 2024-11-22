128e54b91f7ae710a34c8b4dbef4168252da074b
dhoni <alveox@gmail.com>
2024-11-22 20:19:41 +0700

    fix path on traefik.yml fixing how to on readme

--- readme.md
+++ readme.md
@@ -1,8 +1,13 @@
 # nextcloud-aio-traefik
 
-clone this repo https://github.com/techworks-id/nextcloud_aio-traefik.git <br/>
+Clone this repo: <br/>
+git clone https://github.com/techworks-id/nextcloud_aio-traefik.git <br/>
+cd nextcloud_aio-traefik <br/>
 
-run setup.sh for automated container installation or run manualy using several command below. <br/>
+For automated container installation run:<br/>
+bash setup.sh <br/>
+
+or run manualy using several command below. <br/>
 
 sed -i 's/aaa@bbb.ccc/your@email.com/' docker-compose-traefik.yml <br/>
 sed -i 's/hostname.domain.TLD/your.domain.com/' dynamic/nextcloud.yml <br/>
@@ -12,3 +17,6 @@ docker network create --driver=bridge nextcloud-aio <br/>
 
 docker compose -p traefik -f docker-compose-traefik.yml up -d <br/>
 docker compose -p nextcloud_aio -f docker-compose-nextcloud_aio.yml up -d <br/>
+
+to uninstall run: <br/>
+bash uninstall.sh <br/>
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "Removing nextcloud and traefik"
 read -r -p "Are you sure? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-then
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]] ; then
     docker compose -p nextcloud_aio -f docker-compose-nextcloud_aio.yml down
     docker compose -p traefik -f docker-compose-traefik.yml down
     docker ps --filter name=nextcloud-aio* -aq | xargs docker container stop
@@ -11,7 +10,8 @@ then
 
     echo "To prevent lost of importand data, volume must be remove manualy."
     echo "If you sure want to remove all dangling volume, use command: docker volume prune -a"
-    echo "Your Nextcloud data is saved using this environment: "NEXTCLOUD_DATADIR: /mnt/ncdata", If you want to deploy nextcloud-aio again remove /mnt/ncdata or use other path before running setup.sh"
+    echo "Your Nextcloud data is saved using this environment: "NEXTCLOUD_DATADIR: /mnt/ncdata""
+    echo "If you want to deploy nextcloud-aio again remove /mnt/ncdata or use other path before running setup.sh"
 else
     echo "Ok then. :D"
 fi
